919c9b683b44e8e80b448b092af56e39e9db910c
Alice Chen <zixic@student.unimelb.edu.au>
2021-10-08 13:25:37 +1100

    Added stock list/ refresh

--- components/Item.js
+++ components/Item.js
@@ -1,10 +1,104 @@
-import React from 'react';
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { List, Avatar, Divider } from "react-native-paper";
+import * as Linking from "expo-linking";
+import Axios from "axios";
+// https://au.finance.yahoo.com/quote/wow.Ax
 
-export default function Item() {
-    return (
-        <View>
-            
-        </View>
-    )
+export default function Item({ code, isUpdate}) {
+  const [data, setData] = useState("");
+  const [company, setCompany] = useState("");
+  const baseUrl = `https://www.asx.com.au/asx/1/share/${code}`;
+  const baseCompany = `https://www.asx.com.au/asx/1/company/${code}`;
+
+  const shareData = () => {
+    Axios.get(baseUrl)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        alert("not found");
+      });
+  };
+
+  const companyData = () => {
+    Axios.get(baseCompany)
+      .then((response) => {
+        setCompany(response.data);
+      })
+      .catch((error) => {
+        alert("not found");
+      });
+  };
+
+  useEffect(() => {
+    shareData();
+    companyData();
+  },[isUpdate]);
+
+  const titleCase = (s) => {
+    //titleCase(company.name_full)
+    const changed = s
+      .replace(/^[-_]*(.)/, (_, c) => c.toUpperCase()) // Initial char (after -/_)
+      .replace(/[-_]+(.)/g, (_, c) => " " + c.toUpperCase()); // First char after each -/_
+    return changed;
+    };
+    
+ _handlePress = () => {
+   Linking.openURL(`https://au.finance.yahoo.com/quote/${code}.Ax`);
+    };
+    
+  const toRender = () => {
+    if (data && company) {
+      return (
+        <>
+          <TouchableOpacity onPress={_handlePress}>
+            <List.Item
+              title={code}
+              description={titleCase(company.name_abbrev)}
+              left={(props) => (
+                <Avatar.Image
+                  size={70}
+                  style={{ backgroundColor: "white" }}
+                  source={{
+                    uri: `https://files.marketindex.com.au/xasx/96x96-png/${code.toLowerCase()}.png`,
+                  }}
+                />
+              )}
+              right={(props) => (
+                <View>
+                  <Text>{data.last_price}</Text>
+                  <Text
+                    style={
+                      data === undefined
+                        ? null
+                        : {
+                            color:
+                              data.change_in_percent.slice(0, -1) >= 0
+                                ? "green"
+                                : "red",
+                          }
+                    }
+                  >
+                    {data.change_price + " "}
+                    {data.change_in_percent}
+                  </Text>
+                </View>
+              )}
+            />
+          </TouchableOpacity>
+          <Divider />
+        </>
+      );
+    }
+  };
+
+  console.log('rerendered',code)
+
+  return (
+    <View>
+      {toRender()}
+      {/* <Button title="More Information" onPress={_handlePress} /> */}
+    </View>
+  );
 }
--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,84 @@
-import React from 'react';
-import {View} from 'react-native'
+import React, { useState, useEffect } from "react";
+import { View, ScrollView, StyleSheet, RefreshControl,Text } from "react-native";
+import { FAB } from "react-native-paper";
+import Item from "../components/Item";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-    return (
-        <View>
-            
-        </View>
-    )
+  const [refreshing, setRefreshing] = useState(false);
+  const [update, setUpdate] = useState(false);
+  const [showDia, setShowDia] = useState(false);
+  const [codeList, setCodeList] = useState([
+    "QAN",
+    "NAB",
+    "CBA",
+    "MP1",
+    "ANZ",
+    "COL",
+    "APA",
+    "A2M",
+    "WOW",
+    "4DX",
+    "CMW",
+    "VAS",
+  ]);
+
+  // https://www.codegrepper.com/code-examples/javascript/react+sort+array+alphabetically
+  codeList.sort(function (a, b) {
+    return a.localeCompare(b); //using String.prototype.localCompare()
+  });
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setUpdate(!update);
+    setRefreshing(false);
+  };
+
+  const loadData = (stock) => {
+    if (update) {
+      return <Item key={stock} code={stock} isUpdate={update} />;
+    } else {
+      return <Item key={stock} code={stock} />;
+    }
+  };
+
+  const loadDia = () => {
+    if (showDia) {
+      return ()
+    }
+  }
+
+  return (
+    <>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {codeList.map((stock) => {
+          {
+            return loadData(stock);
+          }
+        })}
+      </ScrollView>
+      <FAB
+        icon="plus"
+        onPress={() => setShowDia(true)}
+        style={styles.fab}
+      />
+    </>
+  );
 }
+
+// https://geekscoders.com/courses/react-native/lessons/react-native-floating-action-button/
+const styles = StyleSheet.create({
+  fab: {
+    position: "absolute",
+    margin: 15,
+    right: 0,
+    bottom: 0,
+    flex: 1,
+    backgroundColor: "rgba(245, 40, 145, 1)",
+  },
+});
